b1503efb3204b31d4a0520a1e255409e4c8d0c1b
Ryan Imel <ryan@aspiringindie.com>
2012-09-03 00:52:44 -0400

    Added support for the Co-Authors Plus plugin.

--- functions.php
+++ functions.php
@@ -61,17 +61,32 @@ function founders_login_css() {
 
 
 // Taking over the meta output
+// Supports multiple authors: Display only if the Co-Authors Plus plugin is installed.
+if ( function_exists( 'coauthors_posts_links' ) ) {
 function twentyeleven_posted_on() {
-	printf( __( '<span class="gravatar">%1$s</span><span class="sep ryan">Posted on </span><a href="%2$s" title="%3$s" rel="bookmark"><time class="entry-date" datetime="%4$s" pubdate>%5$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%6$s" title="%7$s" rel="author">%8$s</a></span></span>', 'twentyeleven' ),
-		get_avatar( get_the_author_meta( 'ID' ), 22 ),
+
+	$emails = get_the_coauthor_meta( 'email' );	
+	$gravatars = '';
+	
+	foreach ( $emails as $email ) {
+		$gravatars .= '<span class="gravatar">' . get_avatar( $email, 22 ) . '</span>';
+	}
+	
+	printf( __( '%1$s<span class="sep ryan">Posted on </span><a href="%2$s" title="%3$s" rel="bookmark"><time class="entry-date" datetime="%4$s" pubdate>%5$s</time></a><span class="by-author"> <span class="sep"> by </span>', 'twentyeleven' ),
+		$gravatars,
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'twentyeleven' ), get_the_author() ) ),
-		get_the_author() 
+		esc_attr( sprintf( __( 'View all posts by %s', 'twentyeleven' ), get_the_author() ) )
 	);
-}
+	
+	echo '<span class="author vcard">';
+	
+	coauthors_posts_links();
+	
+	echo '</span></span>';
+} }
 
 ?>
